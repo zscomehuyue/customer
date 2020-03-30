91b787ea20b54f3c6cb1d9a32cd86d3dea1db647
zscomehuyue <zscomehuyue>
2020-03-30 21:28:32 +0800

    config

--- service/src/main/java/com/sxf/cps/customer/CustomerApp.java
+++ service/src/main/java/com/sxf/cps/customer/CustomerApp.java
@@ -2,8 +2,12 @@ package com.sxf.cps.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"com.sxf.cps.customer.domain.merchant.repository"})
 public class CustomerApp {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApp.class, args);
